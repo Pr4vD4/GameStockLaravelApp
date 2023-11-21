d6723f6ed4be74be82ea60aabd9ce57ffa064f43
Pr4vD4 <113354532+Pr4vD4@users.noreply.github.com>
2023-11-21 11:22:29 +0300

    Add files via upload

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -25,7 +25,10 @@
 
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">GameStock</a>
+            <a class="navbar-brand d-flex align-items-center" href="/" >
+                <img src="{{ asset('public/assets/img/logo.svg') }}" style="height: 40px; margin-right: 5px;">
+                GameStock
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
